85962c96cc981aee91c3fac3ed8573ee744d6325
akshith30388 <akshithpola36@gmail.com>
2026-07-24 14:13:07 +0530

    Spring Boot CRUD project

---
+++ src/main/java/com/soa/SoaPratice1Application.java
@@ -0,0 +1,14 @@
+package com.soa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaPratice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaPratice1Application.class, args);
+		System.out.println("Project started Running");
+	}
+
+}
